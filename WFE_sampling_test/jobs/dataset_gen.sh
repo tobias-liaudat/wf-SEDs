031e17e0a8f0e23cca8447b9f9d05f71d80b5a30
Ezequiel Centofanti <ezecentofanti@gmail.com>
2022-04-14 11:59:38 +0200

    added job to generate dataset on candide

---
+++ WFE_sampling_test/jobs/dataset_gen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##########################
+# SMP Script for CANDIDE #
+##########################
+
+# Receive email when job finishes or aborts
+#PBS -M ezequiel.centofanti@cea.fr
+#PBS -m ea
+# Set a name for the job
+#PBS -N dataset_gen
+# Join output and errors in one file
+#PBS -j oe
+# Set maximum computing time (e.g. 5min)
+#PBS -l walltime=48:00:00
+# Request number of cores
+#PBS -l nodes=1:ppn=16
+
+# Activate conda environment
+module load tensorflow/2.7
+
+cd /home/ecentofanti/wf-SEDs/WFE_sampling_test/scripts/
+
+python ./gen-data-multires-parallel.py
+
+# Return exit code
+exit 0
