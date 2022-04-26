3f5c69ffb1043269409e903436e4778b0cf74b39
Ezequiel Centofanti <ezecentofanti@gmail.com>
2022-04-26 11:10:14 +0200

    edit job submit bash file

--- WFE_sampling_test/jobs/train_eval_plot.sh
+++ WFE_sampling_test/jobs/train_eval_plot.sh
@@ -4,24 +4,22 @@
 # SMP Script for CANDIDE #
 ##########################
 
-# Receive email when job finishes or aborts
-#PBS -M ezequiel.centofanti@cea.fr
-#PBS -m ea
+# Receive email when job finishes or aborts (commented)
+# PBS -M ezequiel.centofanti@cea.fr
+# PBS -m ea
 # Set a name for the job
-#PBS -N train_wf-psf
+#PBS -N wf-psf_train
 # Join output and errors in one file
-#PBS -j oe
+# PBS -j oe
 # Set maximum computing time (e.g. 5min)
 #PBS -l walltime=72:00:00
 # Request number of cores (n_machines:ppn=n_cores)
-#PBS -l nodes=n16:ppn=2
+#PBS -l nodes=n03:ppn=2
 
 # Activate conda environment
 module load tensorflow/2.7
 
-cd /home/ecentofanti/wf-SEDs/WFE_sampling_test/scripts/
-
-python ./train-eval-plot-script-click.py \
+python /home/ecentofanti/wf-SEDs/WFE_sampling_test/scripts/train_eval_plot_script_click.py \
     --model param \
     --n_epochs_param 2 2 \
     --n_epochs_non_param 2 2 \
@@ -29,9 +27,9 @@ python ./train-eval-plot-script-click.py \
     --saved_cycle cycle2 \
     --total_cycles 2 \
     --base_id_name _testing_auto_ \
-    --suffix_id_name 2c --suffix_id_name 5c --suffix_id_name 1k --suffix_id_name 2k \
-    --star_numbers 200 --star_numbers 500 --star_numbers 1000 --star_numbers 2000 \
-    --plots_folder plots/testing_plot_folder/ \
+    --suffix_id_name 2k \
+    --star_numbers 2000 \
+    --plots_folder plots/ \
 
 # Return exit code
 exit 0
