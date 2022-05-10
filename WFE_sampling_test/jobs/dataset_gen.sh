5c950a1fd692d5bf158757f356a287844894ae23
Ezequiel Centofanti <ezecentofanti@gmail.com>
2022-05-10 13:49:51 +0200

    Modify bash to generate dataset on candide

--- WFE_sampling_test/jobs/dataset_gen.sh
+++ WFE_sampling_test/jobs/dataset_gen.sh
@@ -5,23 +5,23 @@
 ##########################
 
 # Receive email when job finishes or aborts
-#PBS -M ezequiel.centofanti@cea.fr
-#PBS -m ea
+# PBS -M ezequiel.centofanti@cea.fr
+# PBS -m ea
 # Set a name for the job
-#PBS -N dataset_gen
+#PBS -N dataset_gen_3x
 # Join output and errors in one file
 #PBS -j oe
 # Set maximum computing time (e.g. 5min)
 #PBS -l walltime=72:00:00
 # Request number of cores
-#PBS -l nodes=n17:ppn=32
+#PBS -l nodes=n18:ppn=24
 
 # Activate conda environment
 module load tensorflow/2.7
 
 cd /home/ecentofanti/wf-SEDs/WFE_sampling_test/scripts/
 
-python ./gen-data-multires-parallel.py
+python ./3x-gen-data-multires-parallel.py
 
 # Return exit code
 exit 0
