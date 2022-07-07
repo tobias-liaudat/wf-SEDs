d9cb5c2a4428a7fe389de31c61510d183a15e3c9
Ezequiel Centofanti <ezecentofanti@gmail.com>
2022-07-07 16:05:05 +0200

    Fix click config for multiple valued input parameters.

--- explore_hyperparams/scripts/train_eval_plot_script_click.py
+++ explore_hyperparams/scripts/train_eval_plot_script_click.py
@@ -169,24 +169,28 @@ import click
     "--l_rate_param",
     #nargs=2,
     default=[1e-2, 1e-2],
+    multiple=True,
     type=float,
     help="Learning rates for the parametric parts.")
 @click.option(
     "--l_rate_non_param",
     #nargs=2,
     default=[1e-1, 1e-1],
+    multiple=True,
     type=float,
     help="Learning rates for the non-parametric parts.")
 @click.option(
     "--n_epochs_param",
     #nargs=2,
     default=[20, 20],
+    multiple=True,
     type=int,
     help="Number of training epochs of the parametric parts.")
 @click.option(
     "--n_epochs_non_param",
     #nargs=2,
     default=[100, 120],
+    multiple=True,
     type=int,
     help="Number of training epochs of the non-parametric parts.")
 @click.option(
