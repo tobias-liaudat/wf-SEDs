94efdfe0624ae8c48a457cb57ceaf526061cd633
Ezequiel Centofanti <ezecentofanti@gmail.com>
2022-07-11 16:17:11 +0200

    Modify multicycle parameters click options.

--- explore_hyperparams/jobs/test_multi_cycle.sh
+++ explore_hyperparams/jobs/test_multi_cycle.sh
@@ -8,11 +8,11 @@
 #SBATCH -C v100-32g 
 # /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
 #SBATCH --hint=nomultithread         # hyperthreading desactive
-#SBATCH --time=00:20:00              # temps d'execution maximum demande (HH:MM:SS)
+#SBATCH --time=20:00:00              # temps d'execution maximum demande (HH:MM:SS)
 #SBATCH --output=multi_cycle%j.out  # nom du fichier de sortie
 #SBATCH --error=multi_cycle%j.err   # nom du fichier d'erreur (ici commun avec la sortie)
 #SBATCH -A ynx@gpu                   # specify the project
-#SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
+##SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
 #SBATCH --array=0
 
 # nettoyage des modules charges en interactif et herites par defaut
@@ -28,14 +28,14 @@ opt[0]="--id_name model_multi_cycle_4"
 
 cd $WORK/repos/wf-SEDs/explore_hyperparams/scripts/
 
-srun python -u ./train_eval_plot_script_click.py \
+srun python -u ./train_eval_plot_script_click_multi_cycle.py \
     --d_max_nonparam 5 \
     --n_bins_lda 8 \
     --pupil_diameter 128 \
-    --n_epochs_param 2 --n_epochs_param 2 --n_epochs_param 2 --n_epochs_param 2 \
-    --n_epochs_non_param 5 --n_epochs_non_param 5 --n_epochs_non_param 5 --n_epochs_non_param 5 \
-    --l_rate_non_param 0.1 --l_rate_non_param 0.1 --l_rate_non_param 0.06 --l_rate_non_param 0.06 \
-    --l_rate_param 0.01 --l_rate_param 0.01 --l_rate_param 0.004 --l_rate_param 0.004 \
+    --n_epochs_param_multi_cycle "2 2 2 2" \
+    --n_epochs_non_param_multi_cycle "3 3 3 3" \
+    --l_rate_non_param_multi_cycle "0.1 0.1 0.06 0.06" \
+    --l_rate_param_multi_cycle "0.01 0.004" \
     --model poly \
     --model_eval poly \
     --cycle_def complete \
