ecd5ded14527dc3a4036a6e40f21ad15c46c4d2f
Ezequiel Centofanti <ezecentofanti@gmail.com>
2022-08-05 14:26:12 +0200

    Set option cycle-def to only-parametric.

--- HD_projected_optimisation/jobs/HD_pretrained_param_train.sh
+++ HD_projected_optimisation/jobs/HD_pretrained_param_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=hd_proj   # nom du job
+#SBATCH --job-name=pretrained_param_train   # nom du job
 ##SBATCH --partition=gpu_p2          # de-commente pour la partition gpu_p2
 #SBATCH --ntasks=1                   # nombre total de tache MPI (= nombre total de GPU)
 #SBATCH --ntasks-per-node=1          # nombre de tache MPI par noeud (= nombre de GPU par noeud)
@@ -9,11 +9,11 @@
 # /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
 #SBATCH --hint=nomultithread         # hyperthreading desactive
 #SBATCH --time=20:00:00              # temps d'execution maximum demande (HH:MM:SS)
-#SBATCH --output=hd_proj%j.out  # nom du fichier de sortie
-#SBATCH --error=hd_proj%j.err   # nom du fichier d'erreur (ici commun avec la sortie)
+#SBATCH --output=pretrained_param_train%j.out  # nom du fichier de sortie
+#SBATCH --error=pretrained_param_train%j.err   # nom du fichier d'erreur (ici commun avec la sortie)
 #SBATCH -A ynx@gpu                   # specify the project
 ##SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
-#SBATCH --array=0-19
+#SBATCH --array=0-4
 
 # nettoyage des modules charges en interactif et herites par defaut
 module purge
@@ -24,14 +24,11 @@ module load tensorflow-gpu/py3/2.7.0
 # echo des commandes lancees
 set -x
 
-opt[0]="--id_name _2_cycles_pretrained_train_param_0"
-opt[0]="--id_name _2_cycles_pretrained_train_param_1"
-opt[0]="--id_name _2_cycles_pretrained_train_param_2"
-opt[0]="--id_name _2_cycles_pretrained_train_param_3"
-opt[0]="--id_name _2_cycles_pretrained_train_param_4"
-
-
-
+opt[0]="--id_name _2_cycles_pretrained_param_train_0"
+opt[1]="--id_name _2_cycles_pretrained_param_train_1"
+opt[2]="--id_name _2_cycles_pretrained_param_train_2"
+opt[3]="--id_name _2_cycles_pretrained_param_train_3"
+opt[4]="--id_name _2_cycles_pretrained_param_train_4"
 
 cd $WORK/repos/wf-SEDs/HD_projected_optimisation/scripts/
 
@@ -54,7 +51,7 @@ srun python -u ./train_project_click_multi_cycle.py \
     --saved_cycle cycle2 \
     --model poly \
     --model_eval poly \
-    --cycle_def complete \
+    --cycle_def only-parametric \
     --gt_n_zernikes 45 \
     --d_max_nonparam 5 \
     --saved_model_type checkpoint \
@@ -75,7 +72,7 @@ srun python -u ./train_project_click_multi_cycle.py \
     --model_folder chkp/8_bins/ \
     --log_folder log-files/ \
     --optim_hist_folder optim-hist/ \
-    --base_id_name _2_cycles_pretrained_train_param_ \
+    --base_id_name _2_cycles_pretrained_param_train_ \
     --suffix_id_name 0 --suffix_id_name 1 --suffix_id_name 2 --suffix_id_name 3 --suffix_id_name 4 \
     --star_numbers 0 --star_numbers 1 --star_numbers 2 --star_numbers 3 --star_numbers 4 \
     ${opt[$SLURM_ARRAY_TASK_ID]} \
