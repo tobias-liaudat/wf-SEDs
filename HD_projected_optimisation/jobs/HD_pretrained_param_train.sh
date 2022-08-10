ea988da1b6de4dbd36e1bcb8993bdcc0ca45bf9b
Ezequiel Centofanti <ezecentofanti@gmail.com>
2022-08-10 09:41:08 +0200

    change jobs to develop queue with a 2h time limit.

--- HD_projected_optimisation/jobs/HD_pretrained_param_train.sh
+++ HD_projected_optimisation/jobs/HD_pretrained_param_train.sh
@@ -8,7 +8,7 @@
 #SBATCH -C v100-32g 
 # /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
 #SBATCH --hint=nomultithread         # hyperthreading desactive
-#SBATCH --time=00:02:00              # temps d'execution maximum demande (HH:MM:SS)
+#SBATCH --time=02:00:00              # temps d'execution maximum demande (HH:MM:SS)
 #SBATCH --output=pretrained_param_train%j.out  # nom du fichier de sortie
 #SBATCH --error=pretrained_param_train%j.err   # nom du fichier d'erreur (ici commun avec la sortie)
 #SBATCH -A ynx@gpu                   # specify the project
