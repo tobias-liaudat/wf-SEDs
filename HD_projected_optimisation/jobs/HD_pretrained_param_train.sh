a3dfc1846e78493719c6f2a287b0627b4cb65f16
Ezequiel Centofanti <ezecentofanti@gmail.com>
2022-08-10 09:00:53 +0200

    change jobs to develop queue.

--- HD_projected_optimisation/jobs/HD_pretrained_param_train.sh
+++ HD_projected_optimisation/jobs/HD_pretrained_param_train.sh
@@ -8,11 +8,11 @@
 #SBATCH -C v100-32g 
 # /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
 #SBATCH --hint=nomultithread         # hyperthreading desactive
-#SBATCH --time=20:00:00              # temps d'execution maximum demande (HH:MM:SS)
+#SBATCH --time=00:02:00              # temps d'execution maximum demande (HH:MM:SS)
 #SBATCH --output=pretrained_param_train%j.out  # nom du fichier de sortie
 #SBATCH --error=pretrained_param_train%j.err   # nom du fichier d'erreur (ici commun avec la sortie)
 #SBATCH -A ynx@gpu                   # specify the project
-##SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
+#SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
 #SBATCH --array=0-5
 
 # nettoyage des modules charges en interactif et herites par defaut
